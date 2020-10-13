1924500d32649b0e62fddc207a3cf458b95da11c
Igor F <ig.freid@gmail.com>
2020-10-13 19:24:14 +0600

    added field value

--- database/migrations/2020_10_13_104426_create_currencies_table.php
+++ database/migrations/2020_10_13_104426_create_currencies_table.php
@@ -17,6 +17,7 @@ class CreateCurrenciesTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('code');
+            $table->float('value')->default('1');
             $table->timestamps();
         });
     }
