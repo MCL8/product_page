9b60f46822d0e4c1b4b622c7c512e5f925070e31
Igor F <ig.freid@gmail.com>
2020-10-13 19:26:31 +0600

    /update_rates route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/update_rates', [\App\Http\Controllers\CurrencyController::class, 'update'])->name('updateRates');
