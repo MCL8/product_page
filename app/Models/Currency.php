bdf0b4daedcc0dbc8cfcd70b3e3bd16d53f977c0
Igor F <ig.freid@gmail.com>
2020-10-13 19:25:57 +0600

    added rates parsing function

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Mtownsend\XmlToArray\XmlToArray;
 
 class Currency extends Model
 {
     use HasFactory;
+
+    public static function updateRates()
+    {
+        $ratesXml = file_get_contents('https://nationalbank.kz/rss/rates_all.xml');
+        $ratesArray = XmlToArray::convert($ratesXml);
+
+        if (!isset($ratesArray['channel']['item']) || !is_array($ratesArray['channel']['item'])) {
+            return false;
+        }
+
+        $rubValue = null;
+        foreach ($ratesArray['channel']['item'] as $item) {
+            if ($item['title'] == 'RUB') {
+                $rubValue = (float)$item['description'];
+                break;
+            }
+        }
+
+        if (!$rubValue) {
+            return false;
+        }
+
+        $currencyRub = self::where('code', 'RUB')->firstOrFail();
+
+        $currencyRub->value = $rubValue;
+        return $currencyRub->save();
+    }
 }
