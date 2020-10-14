f73fafc1bc9682ec4d1660dd3b96d54e6234b9b0
Igor F <ig.freid@gmail.com>
2020-10-14 16:20:06 +0600

    fillable / product relation

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['product_id', 'user_id', 'currency', 'amount'];
+
+    public function product()
+    {
+        return $this->hasOne('App\Models\Product', 'id', 'product_id');
+    }
 }
